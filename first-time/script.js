8efdafa993d90e8e29609a3f809cca7d6ea397a6
Cristian <cev@localhost.localdomain>
2018-01-26 10:20:44 -0500

    last

--- first-time/script.js
+++ first-time/script.js
@@ -3,15 +3,28 @@ const ROOT = document.querySelector('#root');
 class helloWorld extends React.Component {
     constructor() {
         super();
-        this.state = {hour: new Date().getHours(), min: new Date().getMinutes(), seconds: new Date().getSeconds() }
-        setInterval(() => this.setState({hour: new Date().getHours(), min: new Date().getMinutes(), seconds: new Date().getSeconds() }), 1000)
+        this.state = {
+            hour: new Date().getHours(),
+            min: new Date().getMinutes(),
+            seconds: new Date().getSeconds()
+        }
+        setInterval(() => this.setState({
+            hour: new Date().getHours(),
+            min: new Date().getMinutes(),
+            seconds: new Date().getSeconds()
+        }), 500)
     }
 
     render() {
-        let h1 = React.createElement("h1", {key: "clock", className: "clock"}, `${this.state.hour}:${this.state.min}:${this.state.seconds}`)
-        return React.createElement("div", {key: "1"}, h1)
+        let h1 = React.createElement("h1", {
+            key: "clock",
+            className: "clock"
+        }, `${this.state.hour}:${this.state.min}:${this.state.seconds}`)
+        return React.createElement("div", {
+            key: "1"
+        }, h1)
     }
 }
 
 
-ReactDOM.render(React.createElement(helloWorld, null), ROOT); 
+ReactDOM.render(React.createElement(helloWorld, null), ROOT);
